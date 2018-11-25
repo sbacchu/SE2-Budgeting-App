7b29a44e58445b7e29c5b69c4a1916d185e9d7ed
Mkr1996 <manojkrk8@gmail.com>
2018-11-24 19:55:40 -0600

    Update main.js

--- BudgetApp/main.js
+++ BudgetApp/main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 850, height: 650 })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
