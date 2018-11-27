c6f9edc623c04387b037d2e266bf14bba35cef8b
davidcooper1 <davidpcooper1@gmail.com>
2018-11-27 00:20:27 -0600

    Changed some form elements on transaction entry modal.

--- BudgetApp/button.js
+++ BudgetApp/button.js
@@ -7,3 +7,10 @@ function CategoryFunc() {
 	document.getElementById("trans").style.display = "none";
 	document.getElementById("categ").style.display = "block";
 }
+
+function processTransaction() {
+	console.log($("#description").val());
+	console.log($("#amount").val());
+	console.log($("#category").val());
+	console.log($("#date").val());
+}
