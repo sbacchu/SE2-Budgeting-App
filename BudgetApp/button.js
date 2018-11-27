265c36392444f7cfcda17b0b2e8b5acf8555af1c
drscorgie <35607265+drscorgie@users.noreply.github.com>
2018-11-26 21:40:48 -0600

    Display Changes with button press
    Main view will switch to the transaction div when the transaction button is pressed. And will switch to the category view when the category button is switched

---
+++ BudgetApp/button.js
@@ -0,0 +1,9 @@
+function TransactionFunc() {
+	document.getElementById("categ").style.display = "none";
+	document.getElementById("trans").style.display = "block";
+}
+
+function CategoryFunc() {
+	document.getElementById("trans").style.display = "none";
+	document.getElementById("categ").style.display = "block";
+}
